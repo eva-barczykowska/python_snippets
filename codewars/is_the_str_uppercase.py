2d3e67839aeb5259e193d76e897f51988263c6fb
e-vah <ewa.barczykowska@t-mobile.cz>
2024-08-07 14:08:23 +0200

    - used the range(), append(), islower(), iterated over a string

---
+++ codewars/is_the_str_uppercase.py
@@ -0,0 +1,25 @@
+"""Task
+Create a method to see whether the string is ALL CAPS.
+
+Examples (input -> output)
+"c" -> False
+"C" -> True
+"hello I AM DONALD" -> False
+"HELLO I AM DONALD" -> True
+"ACSKLDFJSgSKLDFJSKLDFJ" -> False
+"ACSKLDFJSGSKLDFJSKLDFJ" -> True
+
+In this Kata, a string is said to be in ALL CAPS whenever it does not contain
+any lowercase letter so any string containing no letters at all is trivially considered to be
+in ALL CAPS."""
+
+
+def is_uppercase(inp): # taken from chatGPT, doesn't pass ALL codewars tests
+    for char in inp:
+        if char.islower():
+            return False
+        else:
+            return True
+
+
+print(is_uppercase("hello I AM DONALD"))
