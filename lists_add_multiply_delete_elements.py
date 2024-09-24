df544c82b1338e5b41ffa59f19e854235c7ca320
python_snippets <eva.barczykowska@gmail.com>
2024-09-24 09:50:30 +0200

    added info about pop and remove

--- lists_add_multiply_delete_elements.py
+++ lists_add_multiply_delete_elements.py
@@ -44,7 +44,7 @@ print(odd)  # [1, 3, 5, 7, 9, 11]
 
 print('------------')
 # deleting items
-my_list = ['p','r','o','b','l','e','m']
+my_list = ['p', 'r', 'o', 'b', 'l', 'e', 'm']
 
 del my_list[2]    # delete one item (3rd item)
 
@@ -56,3 +56,26 @@ print(my_list)    # Output: ['p', 'm']
 
 del my_list       # delete entire list
 # print(my_list)    # NameError: 'my_list' is not defined
+
+print('Deleting items with remove() and pop() methods:')
+my_list = [1, 2, 3, 4, 5, 6, 7]
+
+# remove item 1 (not item at 1st index)
+my_list.remove(1)
+
+print(my_list)    # Output: [2, 3, 4, 5, 6, 7]
+
+# pop item at index 1
+print(my_list.pop(1))  # Output: 3
+
+# my_list after the item is popped
+print(my_list)    # Output: [2, 4, 5, 6, 7]
+
+# pop last item
+print(my_list.pop())  # Output: 7
+
+# my_list after the last item is popped
+print(my_list)        # Output: [2, 4, 5, 6]
+
+my_list.clear()  # clear list
+print(my_list)   # Output: []
