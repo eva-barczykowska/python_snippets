67fc3fefa8ecee01772655a31ba1af9a0417e9ab
Ewa Barczykowska <eva.barczykowska@gmail.com>
2024-10-03 09:52:16 +0200

    LS OOP exercises

---
+++ LS/oop/_10_default_person.py
@@ -0,0 +1,21 @@
+# Create a class named Person.
+#
+# When you instantiate a Person object, you should pass in one argument that contains the person's name.
+#
+# If no arguments are given, the Person object should be instantiated with a name of "John Doe".
+class Person:
+    def __init__(self, name="John Doe"):
+        self._name = name
+
+    @property # defining getter for name attribute
+    def name(self):
+        return self._name
+
+person1 = Person()
+person2 = Person("Pepe Le Pew")
+
+# Comments show expected output
+print(person1.name)    # John Doe
+print(person2.name)    # Pepe Le Pew
+
+'''We use default argument values, a feature of Python, which lets function arguments have default values.'''
