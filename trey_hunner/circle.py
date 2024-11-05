5e5fb61de519785184ca4cc0627f49d84ac5f894
Ewa Barczykowska <eva.barczykowska@gmail.com>
2024-11-05 12:37:17 +0100

    used decorators to calculate diameter and area on the fly, dynamically

--- trey_hunner/circle.py
+++ trey_hunner/circle.py
@@ -6,8 +6,14 @@ import math
 class Circle:
     def __init__(self, radius=1):
         self.radius = radius
-        self.diameter = self.radius * 2
-        self.area = math.pi * (self.radius ** 2)
+
+    @property
+    def diameter(self):
+        return 2 * self.radius
+
+    @property
+    def area(self):
+        return math.pi * (self.radius ** 2)
 
     def __repr__(self):
         return f"Circle({self.radius})"
