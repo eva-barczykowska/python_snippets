ce6ab9b1648bed40e764bbb29fbd415b153794fa
Ewa Barczykowska <eva.barczykowska@gmail.com>
2024-11-26 12:59:27 +0100

    learning pytest

---
+++ testing_with_pytest/app.py
@@ -0,0 +1,7 @@
+def add_numbers(a, b):
+    """Adds two numbers and returns the result."""
+    return a + b
+
+def divide_numbers(a, b):
+    """Adds two numbers and returns the result."""
+    return a / b
