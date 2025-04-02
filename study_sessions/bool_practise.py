c5e9e0feb1c5745eadc64960fe02ebce8d6d6958
Ewa Barczykowska <eva.barczykowska@gmail.com>
2025-04-02 11:40:10 +0200

    boolean behaviour in python

---
+++ study_sessions/bool_practise.py
@@ -0,0 +1,43 @@
+#print(True and False or True)
+
+# ! comes first
+# than and
+# than or
+
+#print('' or 'Python' and 0)
+# returns last false expression?
+#print(bool('') or bool(0) or bool([]))
+
+#print(not False and not True)
+
+# #print((5 > 3) and ('hello' != 'world'))
+
+# print(5 > 3 and 'hello' != 'world')
+
+#print('abc' > 'abd' or True)
+
+#x = 10
+#print(x > 5 and x < 15 or x == 20)
+
+# print(bool([]) == False and bool('False') == True)
+
+# print(None and 'Skip' or 'Proceed')
+# None is False so it goes to the 2nd
+# or returns the last truthy value
+# if both are false, return the last false value
+# and gives me the first falsey or the last truthy
+# or gives me the first truthy or whatever 2nd
+
+#print((False or []) and ('B' < 'b') or None)
+
+# for AND it returns the firt falsey value it finds
+# or last truthy
+# with OR if the first operand is falsey, it returns the 2nd
+
+# a = 0
+# b = 1
+# print(a or b and not a)
+
+# print('' and None or [] and 'python' or 0)
+
+# print(not (0 or '') and (True and 'hello'))
