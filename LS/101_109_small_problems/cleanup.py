7750d5ab5d853589b3dd78f16bf00c48ee1a1fb8
Ewa Barczykowska <eva.barczykowska@gmail.com>
2025-06-04 11:06:00 +0200

    -creating new string char by char -adding cicd file to .gitignore

--- LS/101_109_small_problems/cleanup.py
+++ LS/101_109_small_problems/cleanup.py
@@ -17,22 +17,91 @@
 # = for loop to iterate over the first string
 # - check isalpha.
 # - if is alpha, add to the empty string
-# - if is not alpha, it needs to add a space to the emptry string
+# - if is not alpha, it needs to add a space to the empty string
 # if more than two non alphas, pass?
 # if more than two non alphas,
 
-def clean_up(string):
-    cleaned_string = ''
-    for i, char in enumerate(string):
-        if char.isalpha():
-            cleaned_string += char
-        elif i == 0 or cleaned_string[-1] != ' ':
-            cleaned_string += ' '
+# def clean_up(string):
+#     cleaned_string = ''
+#     for i, char in enumerate(string):
+#         if char.isalpha():
+#             cleaned_string += char
+#         elif i == 0 or cleaned_string[-1] != ' ':
+#             cleaned_string += ' '
+#
+#
+#     return cleaned_string
+#
+# print(clean_up("---what's my +*& line?") == " what s my line ")
+# # True
+# print(clean_up("Hi Ewa!$") == "Hi Ewa ") #True
 
 
-    return cleaned_string
+# again:
 
-print(clean_up("---what's my +*& line?") == " what s my line ")
-# True
+# - iterate over the string
+# - look at each character
+# - if it's alphabet, add it to the result string
+# - if it's not alphabet, check if the result string ends with a space
+
+# def clean_up(string):
+#     cleaned_string = ''
+#     for char in string:
+#         if char.isalpha():
+#             cleaned_string += char
+#         elif cleaned_string and cleaned_string[-1]!='':
+#             cleaned_string +=''
+#     return cleaned_string
+
+# print(clean_up("---what's my +*& line?") == " what s my line ")
+# # True
+# print(clean_up("Hi Ewa!$") == "Hi Ewa ") #True
+
+
+# print('---')
+#
+# print(clean_up("---what's my +*& line?") == " what s my line ")
+# # True
+# print(clean_up("Hi Ewa!$") == "Hi Ewa ") #True
+
+# overal theory:
+# iterate over the string and build a new one char by char
+#details:
+# - look at each character
+# - if it's alphabet char, add it to the result string
+# - if it's not alphabet
+# --check if str is empty, if it is, add space
+# -- or check if str[-1] is a space, then do NOT add this character
+# --else if it is an alphabetic char, add this char to the str
+
+def clean_up(s):
+    result = ''
+    for char in s:
+        if not char.isalpha():
+            if not result:
+                result += ' '
+            elif result[-1] != ' ':
+                result += ' '
+        else:
+            result += char
+    print(result)
+    return result
+
+print(clean_up("---what's my +*& line?") == " what s my line ")# True
 print(clean_up("Hi Ewa!$") == "Hi Ewa ") #True
 
+#refactor
+def clean_up(s):
+    result = ''
+
+    for char in s:
+        if not char.isalpha():
+            if not result or result[-1] != ' ':
+                result += ' '
+        else:
+            result += char
+    print(result)
+    return result
+
+print(clean_up("---what's my +*& line?") == " what s my line ")# True
+print(clean_up("Hi Ewa!$") == "Hi Ewa ") #True
