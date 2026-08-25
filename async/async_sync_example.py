b1aea58f5d8af357085eedcec42f055c946ffdf2
Ewa Barczykowska <eva.barczykowska@gmail.com>
2026-08-25 15:17:18 +0200

    used async but this is not really async! this will not speed anything up coz we need to use await

--- async/async_sync_example.py
+++ async/async_sync_example.py
@@ -16,25 +16,64 @@
 # group = asyncio.gather(*tasks)
 # loop.run_until_complete(group)
 
+#
+# import warnings
+# warnings.filterwarnings("ignore")
+#
+# import requests
+# import time
+#
+# urls = ['https://nytimes.com',
+#         'https://python.org',
+#         'https://us.pycon.org']
+#
+# sizes = {}
+#
+# start_time = time.time()
+#
+# for one_url in urls:
+#     print(one_url)
+#     content = requests.get(one_url).content
+#     sizes[one_url] = len(content)
+#
+# total_time = time.time() - start_time
+# print(f'It took {total_time} seconds') # It took 1.6350290775299072 seconds
+
+# same but with asyncio, but this will not work
+#!/usr/bin/env python3
 
 import warnings
 warnings.filterwarnings("ignore")
-
-import requests
 import time
+import requests
+import asyncio
+
 
 urls = ['https://nytimes.com',
-        'https://python.org',
-        'https://us.pycon.org']
+            'https://python.org',
+            'https://us.pycon.org']
 
 sizes = {}
 
-start_time = time.time()
 
+async def measure_url_content(one_url):
+        print(one_url)
+        content = requests.get(one_url).content
+        sizes[one_url] = len(content)
+
+
+loop = asyncio.get_event_loop()
+
+start_time = time.time()
 for one_url in urls:
-    print(one_url)
-    content = requests.get(one_url).content
-    sizes[one_url] = len(content)
+    loop.create_task(measure_url_content(one_url))
+
+tasks = asyncio.all_tasks(loop=loop)
+group = asyncio.gather(*tasks)
+loop.run_until_complete(group)
 
 total_time = time.time() - start_time
-print(f'It took {total_time} seconds')
+print(f'It took {total_time} seconds') #It took 2.7863478660583496 seconds so it is NOT FASTER AT ALL
+
+
+
